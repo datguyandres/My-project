2a10f0564793c3cf85fd7f669c202ab677772111
datguyandres <spartansup18@gmail.com>
2023-04-29 15:22:13 -0700

    added all scripts

---
+++ Assets/CoinCollecting.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollecting : MonoBehaviour
+{
+public GameObject player;
+void OnTriggerEnter(Collider collision)
+    {
+        
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject.name == "PlayerCapsule")
+        {
+            //DestroyObject(gameObject);
+            Object.Destroy(gameObject);
+            player.GetComponent<PlayerScore>().AddScore();
+           
+           Debug.Log("Do something here");
+        }
+
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+    }
+}
---
+++ Assets/PlayerScore.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScore : MonoBehaviour
+{
+    public int playerscore = 0;
+    public void AddScore(){
+        playerscore++;
+    }
+}
